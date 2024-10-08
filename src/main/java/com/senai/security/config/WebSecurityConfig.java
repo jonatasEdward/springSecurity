a40b6fdfeb646e7cc703ca70a857f03c7014cd4b
jonatas.oliveira <jonatas.oliveira@fornecedores.goias.gov.br>
2024-10-07 23:11:40 -0300

    - Configuração de SecurityFilterChain - Remoção dos PreAuthorize

--- src/main/java/com/senai/security/config/WebSecurityConfig.java
+++ src/main/java/com/senai/security/config/WebSecurityConfig.java
@@ -2,17 +2,36 @@ package com.senai.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/login").permitAll()
+                        .requestMatchers("/admin").hasAnyRole("ADMIN")
+                        .requestMatchers("/user").hasAnyRole("ADMIN", "USER")
+                        .anyRequest().authenticated()
+                ).formLogin(Customizer.withDefaults());
+        return http.build();
+    }
 
     @Bean
     public UserDetailsService userDetailsService() {
--- src/main/java/com/senai/security/controller/WelcomController.java
+++ src/main/java/com/senai/security/controller/WelcomController.java
@@ -1,6 +1,5 @@
 package com.senai.security.controller;
 
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,13 +12,11 @@ public class WelcomController {
     }
 
     @GetMapping("/user")
-    @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
     public String user() {
         return "Authorize user";
     }
 
     @GetMapping("/admin")
-    @PreAuthorize("hasRole('ADMIN')")
     public String admin() {
         return "Authorize admin";
     }
